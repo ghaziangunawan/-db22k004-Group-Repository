c8c81dc18851dd994eaae9fa047bf6132984cf50
GhazianG <87701299+GhazianG@users.noreply.github.com>
2023-01-03 15:21:23 +0300

    Update settings.py

--- project_django/settings.py
+++ project_django/settings.py
@@ -124,6 +124,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3d64.up.railway.app/']
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
